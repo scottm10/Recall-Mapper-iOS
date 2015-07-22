eae3b12106e3eea8e98e1f462f577ad35ef40c48
Scott Magee <thetruehalfbloodprince@gmail.com>
2015-07-22 10:17:42 -0400

    Initial Commit

---
+++ Recall Mapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recall Mapper
+//
+//  Created by Scott Magee on 7/22/15.
+//  Copyright © 2015 Scott Magee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
