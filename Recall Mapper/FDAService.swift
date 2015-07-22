14eef7fbf6546e90689f7fa3e9594b5267336bc1
Scott Magee <thetruehalfbloodprince@gmail.com>
2015-07-22 13:06:56 -0400

    Fetch Disclaimer from JSON Object
    Wrote code to reach out to api.fda.gov and get data. Then to write the disclaimer on screen once data is fetched. Also, turned off ATS to communicate with the site.

---
+++ Recall Mapper/FDAService.swift
@@ -0,0 +1,54 @@
+//
+//  FDAService.swift
+//  Recall Mapper
+//
+//  Created by Scott Magee on 7/22/15.
+//  Copyright © 2015 Scott Magee. All rights reserved.
+//
+
+import Foundation
+
+let DRUG_ENFORCEMENT_URL = "https://api.fda.gov/drug/enforcement.json";
+let API_KEY = "W0AOLQFs6HbOkZD8uIcELIWpxg7kTCAywBcGfDVA"
+let BEGIN_QUERY = "?api_key=\(API_KEY)"
+
+class FDAService {
+    
+    weak var ref:ViewController!
+    
+    /**
+    *
+    **/
+    func executeApiQuery(url:NSURL) -> Void {
+        let theRequest = NSURLRequest(URL: url)
+        
+        
+        let session = NSURLSession.sharedSession()
+        let dataTask = session.dataTaskWithRequest(theRequest, completionHandler: {(possibleData:NSData?, response:NSURLResponse?, possibleError:NSError?) -> Void in
+            if let data = possibleData{
+                if data.length > 0 && possibleError == nil {
+                    do {
+                        let something: AnyObject? = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)
+                        if let actualInfo = something {
+                            if let dict = actualInfo as? NSDictionary{
+                                if let metaDict = dict.objectForKey("meta") {
+                                    if let disclaimer = metaDict.objectForKey("disclaimer") as? String {
+                                        dispatch_async(dispatch_get_main_queue(),{
+                                            self.ref.updateLabelText(disclaimer)})
+                                    }
+                                }
+                            }
+                        }
+                    } catch _ {
+                        
+                    }
+                }
+            } else {
+                print(possibleError?.description)
+            }
+        })
+        
+        dataTask.resume()
+    }
+    
+}
--- Recall Mapper/ViewController.swift
+++ Recall Mapper/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,20 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func updateLabelText(text:String) -> Void{
+        self.testLabel.text = text
+        self.testLabel.setNeedsLayout()
+    }
+    
+    @IBAction func callAPI() -> Void {
+        let service = FDAService()
+        service.ref = self;
+        let testUrl = NSURL(string: "https://api.fda.gov/drug/enforcement.json?search=_exists_:city+AND+_exists_:state")
+        if let url = testUrl {
+            service.executeApiQuery(url)
+        }
     }
 
 
